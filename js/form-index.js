1bc6b03f545184fdd31378685f2760f2e61b10fc
Alexander <alexsvistunov73@gmail.com>
2016-09-12 17:50:40 +0300

    добавил анимацию

--- js/form-index.js
+++ js/form-index.js
@@ -1,7 +1,9 @@
 var link = document.querySelector("section .button-hotels");
 var popup = document.querySelector(".form-index");
+var date = popup.querySelector("[name=date-in]");
 
 link.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.toggle("form-index-show");
-});
+    date.focus();
+ });
