d9ffd84fb00bca2e82346322a29dd4edd1492a26
Alexander Svistunov <alexsvistunov73@gmail.com>
2017-08-08 22:18:32 +0300

    index code review: add flexbox containers in form elements

--- js/index-form.js
+++ js/index-form.js
@@ -1,9 +1,10 @@
+// открытие/закрытие формы
 var link = document.querySelector("section .button-hotels");
 var popup = document.querySelector(".form-index");
 var date = popup.querySelector("[name=date-in]");
 
 link.addEventListener("click", function(event) {
     event.preventDefault();
-    popup.classList.toggle("form-index-show");
+    popup.classList.toggle("active");
     date.focus();
  });
