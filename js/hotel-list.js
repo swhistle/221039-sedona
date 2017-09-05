4e4dd6b9a84782df8410dd1d7f2eb327841931a9
Alexander Svistunov <alexsvistunov73@gmail.com>
2017-09-05 14:03:21 +0300

    add setTimeout for hanging src-request and clearTimeout for success one

--- js/hotel-list.js
+++ js/hotel-list.js
@@ -1,5 +1,6 @@
-// (function() {
+(function() {
   var container = document.querySelector(".hotels");
+  var IMAGE_TIMEOUT = 10000;
 
   hotels.forEach(function(hotel) {
     var element = getElementFromTemplate(hotel);
@@ -21,6 +22,26 @@
     element.querySelector(".hotel-price").textContent = "от " + data.price + " Р.";
     element.querySelector(".hotel-rating").textContent = "Рейтинг: " + data.rating;
     element.querySelector(".hotel-rating").classList.add("hotel-rating-star-" + data.stars);
+
+    var hotelImageContainer = element.querySelector(".hotel-image-container");
+    var hotelImage = new Image();
+
+    var imageLoadTimeOut = setTimeout(function() {
+      hotelImage.src = "";
+      hotelImageContainer.classList.add("hotel-nophoto");
+    }, IMAGE_TIMEOUT);
+
+    hotelImage.onload = function() {
+      clearTimeout(imageLoadTimeOut);
+    }
+
+    hotelImage.onerror = function() {
+      hotelImageContainer.classList.add("hotel-nophoto");
+    }
+
+    hotelImage.src = data.img;
+    hotelImageContainer.appendChild(hotelImage);
+
     return element;
   }
-// })();
+})();
